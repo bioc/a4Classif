7f314d7fec14027aae18e0d63f0c851ec8db9868
Tobias Verbeke <tobias.verbeke@openanalytics.eu>
2011-10-20 06:09:55 +0000

    add package startup message
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/a4Classif@59446 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname){
+  options(error = NULL)
+  packageStartupMessage(paste("\na4Classif version ", packageDescription("a4Classif")$Version, 
+          "\n", sep = ""))
+}
+
